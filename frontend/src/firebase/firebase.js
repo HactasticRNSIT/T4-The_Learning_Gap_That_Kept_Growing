ba9794e05e99796a2c78d29d7226d3ef369da258
Chaitanya20076 <chaitanya222007@gmail.com>
2026-05-09 10:36:51 +0530

    Installed firebase nefore deploy

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyB5Z0XHTTbizCV_irSeXQv2Vo7V5fKl580',
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'astra-learn-706cb.firebaseapp.com',
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'astra-learn-706cb',
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'astra-learn-706cb.firebasestorage.app',
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '391960859686',
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:391960859686:web:286c69f9451169d68242e7',
-}
+  apiKey: "AIzaSyAPwIRgJx7MBxmZ4X3N3eZ26C3rvGi8vSA",
+  authDomain: "astra-learn-4fc01.firebaseapp.com",
+  projectId: "astra-learn-4fc01",
+  storageBucket: "astra-learn-4fc01.firebasestorage.app",
+  messagingSenderId: "629150572513",
+  appId: "1:629150572513:web:bbb0445f2ee4085b8bbca0"
+};
 
 const app = initializeApp(firebaseConfig)
 
